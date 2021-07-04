d6367d63bb57ca6f82178ed74615f930d0a32535
anuj26298 <kumar26298anuj@gmail.com>
2021-07-04 13:49:54 +0530

    ADDED Welcome message

---
+++ src/main/java/com/addressbook/controller/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.addressbook.controller;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("=======        Address Book        ======");
+        System.out.println("=========================================");
+    }
+}
